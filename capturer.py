11fd6de171c3d91840a8e8db4419c33fc2278213
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-04-17 11:57:44 -0400

    almost getting there

--- capturer.py
+++ capturer.py
@@ -7,6 +7,7 @@ import concurrent.futures
 
 import numpy as np
 import cv2
+import keras
 
 from mss import mss, tools
 
@@ -56,35 +57,39 @@ def capture_image(screen_postion, delay, thumbnail_size, name_q, at_bat_q):
             img = np.array(sct.grab(screen_postion))
 
             resized = cv2.resize(img, thumbnail_size)
-            timestamp = datetime.now().strftime('%m_%d-%H-%M-%S')
+            timestamp = datetime.now().strftime('%m_%d-%H-%M-%S-%f')
 
             name_q.put((resized, timestamp))
             at_bat_q.put((resized, timestamp))
 
-        print("timestamp")
+        print(timestamp)
         time.sleep(delay)
 
-def parser(model_path, base_folder, image_q, title_q):
-    model = load_model(model_path)
+def parser(model_path, image_q, title_q):
+    print("Loading model {} : {}".format(model_path, datetime.now().strftime('%H:%M:%S')))
+    model = keras.models.load_model(model_path)
+    print("Loaded model {} : {}".format(model_path, datetime.now().strftime('%H:%M:%S')))
 
     while True:
          (img, timestamp) = image_q.get()
          prediction = model.predict_classes(img)
 
          if prediction == 0:
-             filename = '{}/{}.png'.format(base_folder, timestamp)
+             filename = '{}{}.png'.format(base_folder, timestamp)
              cv2.imwrite(filename, img)
              title_q.put(timestamp)
 
 def record_mapping(output, at_bat_title_q, name_title_q):
 
-    player_name = name_title_q.get()
-    while True:
-        player_name = name_title_q.get() if not name_title_q.empty() else player_name
+    with open(output, "a") as f:
+        player_name = name_title_q.get()
+        while True:
+            if not name_title_q.empty():
+                player_name = name_title_q.get()
 
-        if not at_bat_title_q.empty():
-            at_bat_img_title = at_bat_title_q.get()
-            output.write("{},{}\n".format(player_name, at_bat_img_title))
+                if not at_bat_title_q.empty():
+                    at_bat_img_title = at_bat_title_q.get()
+                    f.write("{},{}\n".format(player_name, at_bat_img_title))
 
 
 def start_pipeline(delay, thumbnail_size):
@@ -103,13 +108,25 @@ def start_pipeline(delay, thumbnail_size):
     base_folder_batter = "./matchups/batters/"
     base_folder_batter = "./matchups/batters/"
 
+    record_mapping = "./record.csv"
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        image_capture_executor = executor.submit(capture_image, delay, thumbnail_size, name_image_q, at_bat_image_q)
         at_bat_executor = executor.submit(parser, at_bat_model, at_bat_image_q, at_bat_title_q)
         name_executor = executor.submit(parser, name_model, name_image_q, name_title_q)
         recorder_executor = executor.submit(record_mapping, at_bat_title_q, name_title_q)
 
+        """
+        image_capture_executor = executor.submit(capture_image,
+                                                 SCREEN_POSITIONS.get('monitor_top_left'),
+                                                 delay,
+                                                 thumbnail_size,
+                                                 name_image_q,
+                                                 at_bat_image_q)
+        """
+        executor.shutdown()
 
 
 if __name__ == "__main__":
-    start_pipeline(0.4, (300, 300))
+    at_bat_model = "./models/at_bat_net.hdf5"
+    model = keras.models.load_model(at_bat_model)
+#    start_pipeline(0.4, (300, 300))
