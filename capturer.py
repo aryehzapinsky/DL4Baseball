38ea7a7d2946c70bb5c0e38d12327823bc145c4c
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-04-18 00:30:16 -0400

    added specific tf graphs

--- capturer.py
+++ capturer.py
@@ -11,6 +11,8 @@ import threading
 import numpy as np
 import cv2
 import keras
+from keras import backend as K
+import tensorflow as tf
 
 from mss import mss, tools
 
@@ -75,23 +77,27 @@ def capture_image(screen_postion, delay, thumbnail_size, name_q, at_bat_q):
 
 def parser(model_path, base_folder, image_q, title_q):
     print("Loading model {} : {}".format(model_path, datetime.now().strftime('%H:%M:%S')))
-    model = keras.models.load_model(model_path)
-    print("Loaded model {} : {}".format(model_path, datetime.now().strftime('%H:%M:%S')))
+    graph = tf.Graph()
+    with graph.as_default():
+        session = tf.Session()
+        with session.as_default():
+            model = keras.models.load_model(model_path)
+            print("Loaded model {} : {}".format(model_path, datetime.now().strftime('%H:%M:%S')))
 
-    while True:
-        print(image_q.qsize())
-        (img, timestamp) = image_q.get()
-        print("Setting up prediction || {} - {}".format(model_path, timestamp))
-        prediction = model.predict_classes(img)[0][0]
-        print("Predicted || {} - {} ~~~~~~~~~~ Predicted class: {}".format(model_path, timestamp, prediction))
+            while True:
+                print(image_q.qsize())
+                (img, timestamp) = image_q.get()
+                print("Setting up prediction || {} - {}".format(model_path, timestamp))
+                prediction = model.predict_classes(img)[0][0]
+                print("Predicted || {} - {} ~~~~~~~~~~ Predicted class: {}".format(model_path, timestamp, prediction))
 
-        if prediction == 0:
-            filename = '{}{}.png'.format(base_folder, timestamp)
-            print("FILENAME: {}<<<<<<<<<".format(filename))
-            cv2.imwrite(filename, img[0])
-            title_q.put(timestamp)
+                if prediction == 0:
+                    filename = '{}{}.png'.format(base_folder, timestamp)
+                    print("FILENAME: {}<<<<<<<<<".format(filename))
+                    cv2.imwrite(filename, img[0])
+                    title_q.put(timestamp)
 
-        print("*******************")
+                print("*******************")
 
 def record_mapping(output, at_bat_title_q, name_title_q):
 
@@ -170,9 +176,9 @@ def start_pipeline(delay, thumbnail_size):
     capture_thread = threading.Thread(target=capture_image, name="capture",
                                       args=(SCREEN_POSITIONS.get('monitor_top_left'), delay, thumbnail_size, name_image_q, at_bat_image_q))
 
-    name_thread.start()
-    time.sleep(5)
     at_bat_thread.start()
+    time.sleep(5)
+    name_thread.start()
     time.sleep(5)
     capture_thread.start()
     recorder_thread.start()
